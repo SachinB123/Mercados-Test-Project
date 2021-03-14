903fde846e027f8bb45234483057a7d19dc2e9d9
SachinB123 <sachinbadola20682@gmail.com>
2021-03-14 18:18:01 +0530

    Added service to fetch and update data from node js server

--- angular-frontend/src/app/services/my-service.service.ts
+++ angular-frontend/src/app/services/my-service.service.ts
@@ -5,10 +5,17 @@ import {HttpHeaders, HttpClient, HttpResponse} from '@angular/common/http';
   providedIn: 'root'
 })
 export class MyServiceService {
+  private _hostName;
+  private _serverBaseUrl;
 
-  constructor(private http: HttpClient) { }
-
-  
+  constructor(private http: HttpClient) {
+    this._hostName = window.location.hostname;
+    if (this._hostName === 'localhost' || this._hostName === '127.0.0.1') {
+        this._serverBaseUrl = 'http://localhost:3000';
+    } else {
+        this._serverBaseUrl = 'window.location.origin';
+    }
+  } 
 
   getBackendData() {
     let headers = new HttpHeaders(); // since HttpHeaders is immutable
@@ -18,8 +25,21 @@ export class MyServiceService {
     headers = headers.append('Content-Control', 'no-cache');
     headers = headers.append('Pragma', 'no-cache');
 
-    // const url = 'http://localhost:3000/api/test'; // only local
-    const url = window.location.origin + '/.netlify/functions/server' + '/api/test';
+    // const url = 'http://localhost:3000/.netlify/functions/server/api/getcsvdata'; // only local
+    const url = this._serverBaseUrl + '/.netlify/functions/server' + '/api/getcsvdata';
     return this.http.get(url, {headers});
+  }
+
+  updateBackendData(data) {
+    let headers = new HttpHeaders(); // since HttpHeaders is immutable
+    headers = headers.append('Content-Type', 'application/json');
+
+    /* Added for IE11 API cache and not refecting changes Issue*/
+    headers = headers.append('Content-Control', 'no-cache');
+    headers = headers.append('Pragma', 'no-cache');
+
+    // const url = 'http://localhost:3000/.netlify/functions/server/api/setcsvdata'; // only local
+    const url = this._serverBaseUrl + '/.netlify/functions/server' + '/api/getcsvdata';
+    return this.http.post(url, data, {headers});
   }
 }
