b6553bb9ee12bdf2b48a89bba6d31b7b022a4754
SachinB123 <sachinbadola20682@gmail.com>
2021-03-14 20:13:42 +0530

    Added test for lambda

--- express/server.js
+++ express/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 const router = express.Router();
 
 /*See why we need LAMBDA_TASK_ROOT for netlify - https://answers.netlify.com/t/hosting-a-file-along-with-my-function/1527/19 */
-const resolvedLAMBDAPath = (process.env.LAMBDA_TASK_ROOT)? path.resolve(process.env.LAMBDA_TASK_ROOT, 'sales_data_ABC.csv'):path.resolve(__dirname, '../backend-asset', 'sales_data_ABC.csv')
+const resolvedLAMBDAPath = (process.env.LAMBDA_TASK_ROOT)? path.resolve(process.env.LAMBDA_TASK_ROOT, '../backend-asset', 'sales_data_ABC.csv'):path.resolve(__dirname, '../backend-asset', 'sales_data_ABC.csv')
 router.get("/api/getcsvdata", (req, res) => {
 
     let idArray = [];
