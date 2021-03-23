ac3fa0ddcfbc4947226b21a7acaba3e220e02bb5
SachinB123 <sachinbadola20682@gmail.com>
2021-03-23 12:04:02 +0530

    Made cchanges

--- express/server.js
+++ express/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const csv = require('fast-csv');
 
 const app = express();
-const cors = require("cors");
+// const cors = require("cors");
 // app.use(cors()); // only on local
 const apiRoute = "/.netlify/functions/server"; // only on for netlify
 
@@ -19,52 +19,52 @@ const router = express.Router();
 
 /*See why we need LAMBDA_TASK_ROOT for netlify - https://answers.netlify.com/t/hosting-a-file-along-with-my-function/1527/19 */
 const fileName = "./functions/sales_data_ABC.csv";
-const resolvedLAMBDAPath = (process.env.LAMBDA_TASK_ROOT)? path.resolve(__filename):path.resolve(__dirname, fileName)
+const resolvedLAMBDAPath = (process.env.LAMBDA_TASK_ROOT)? path.resolve(__filename):path.resolve(__dirname, '../backend-asset', 'sales_data_ABC.csv')
 router.get("/api/getcsvdata", (req, res) => {
 
     let idArray = [];
-    res.status(200)
-                .json({
-                    // message: 'Original CSV data fetch successfull',
-                    // data: idArray
-                    lambdaTaskRoot: process.env.LAMBDA_TASK_ROOT,
-                    dirnameL: path.resolve('sales_data_ABC.csv'),
-                    fileNameL: __filename,
-                    allInCurrentDir:fs.readdirSync(__dirname),
-                    // allInCurrentDir2:fs.readFileSync('sales_data_ABC.csv'),SS
-                    allInLAMBDAROOT:fs.readdirSync(process.env.LAMBDA_TASK_ROOT),
-                    allInLAMBDAROOTSRC:fs.readdirSync(process.env.LAMBDA_TASK_ROOT+'/src'),
-                    allInLAMBDAROOTSRC2:fs.readdirSync('/var/task/src/'),
-                    // allInTMP1:fs.readdirSync('./tmp'),
-                    // allInTMP2:fs.readdirSync('./tmp/'),
-                    allInOpt1:fs.readdirSync('/opt/'),
-                    allInSrv:fs.readdirSync('/srv/'),
-                    // allInRoot:fs.readdirSync('/root/'),
-                    allInDev:fs.readdirSync('/dev/'),
-                    allInBin:fs.readdirSync('/bin/'),
-                    allInMedia:fs.readdirSync('/media/')
-
-                });
-    
-    // fs.createReadStream(resolvedLAMBDAPath)
-    //     .pipe(csv.parse({
-    //         headers: true
-    //     }))
-    //     .on('error', error => console.error(error))
-    //     .on('data', row => {
-    //         console.log(row);
-    //         idArray.push(row); //Add it to the array
-    //     })
-    //     .on('end', rowCount => {
-    //         console.log(`Parsed ${rowCount} rows`);
-
-    //         res
-    //             .status(200)
+    // res.status(200)
     //             .json({
-    //                 message: 'Original CSV data fetch successfull',
-    //                 data: idArray
+    //                 // message: 'Original CSV data fetch successfull',
+    //                 // data: idArray
+    //                 lambdaTaskRoot: process.env.LAMBDA_TASK_ROOT,
+    //                 dirnameL: path.resolve('sales_data_ABC.csv'),
+    //                 fileNameL: __filename,
+    //                 allInCurrentDir:fs.readdirSync(__dirname),
+    //                 // allInCurrentDir2:fs.readFileSync('sales_data_ABC.csv'),SS
+    //                 allInLAMBDAROOT:fs.readdirSync(process.env.LAMBDA_TASK_ROOT),
+    //                 allInLAMBDAROOTSRC:fs.readdirSync(process.env.LAMBDA_TASK_ROOT+'/src'),
+    //                 allInLAMBDAROOTSRC2:fs.readdirSync('/var/task/src/'),
+    //                 // allInTMP1:fs.readdirSync('./tmp'),
+    //                 // allInTMP2:fs.readdirSync('./tmp/'),
+    //                 allInOpt1:fs.readdirSync('/opt/'),
+    //                 allInSrv:fs.readdirSync('/srv/'),
+    //                 // allInRoot:fs.readdirSync('/root/'),
+    //                 allInDev:fs.readdirSync('/dev/'),
+    //                 allInBin:fs.readdirSync('/bin/'),
+    //                 allInMedia:fs.readdirSync('/media/')
+
     //             });
-    //     });
+    
+    fs.createReadStream(resolvedLAMBDAPath)
+        .pipe(csv.parse({
+            headers: true
+        }))
+        .on('error', error => console.error(error))
+        .on('data', row => {
+            console.log(row);
+            idArray.push(row); //Add it to the array
+        })
+        .on('end', rowCount => {
+            console.log(`Parsed ${rowCount} rows`);
+
+            res
+                .status(200)
+                .json({
+                    message: 'Original CSV data fetch successfull',
+                    data: idArray
+                });
+        });
 });
 
 router.post("/api/setcsvdata", (req, res) => {
